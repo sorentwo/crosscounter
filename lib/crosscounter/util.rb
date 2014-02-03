dc00329bcac8ddd8847c7b09bfbdb71eb4dd48d8
Parker Selbert <parker@sorentwo.com>
2014-02-03 16:50:03 -0600

    Set the string separator for the whole module

--- lib/crosscounter/util.rb
+++ lib/crosscounter/util.rb
@@ -1,8 +1,18 @@
 require 'set'
 
 module Crosscounter
+  DEFAULT_SEPARATOR = '|'
+
   module Util
     extend self
+
+    def separator=(sep)
+      @@separator = sep
+    end
+
+    def separator
+      @@separator ||= DEFAULT_SEPARATOR
+    end
 
     def hashify(hash)
       stringify(hash).inject({}) do |memo, key|
@@ -11,7 +21,7 @@ module Crosscounter
       end
     end
 
-    def stringify(hash, sep = '|')
+    def stringify(hash, sep = separator)
       hash.flat_map do |key, value|
         if value.kind_of?(Enumerable)
           value.map { |elem| "#{key}#{sep}#{elem}" }
--- spec/crosscounter/util_spec.rb
+++ spec/crosscounter/util_spec.rb
@@ -1,6 +1,17 @@
 require 'crosscounter/util'
 
 describe Crosscounter::Util do
+  describe '.separator=' do
+    after do
+      Crosscounter::Util.separator = Crosscounter::DEFAULT_SEPARATOR
+    end
+
+    it 'sets the separator for future usage' do
+      Crosscounter::Util.separator = '.::.'
+      expect(Crosscounter::Util.separator).to eq('.::.')
+    end
+  end
+
   describe '.stringify' do
     it 'unzips a hash into key-value strings' do
       expect(Crosscounter::Util.stringify(age: [18, 19, 20])).to eq([
